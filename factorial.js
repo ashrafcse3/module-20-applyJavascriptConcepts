35da7cde06ca3dfb3447504f35610dfca16ac9e2
Ashraful Alam <ashrafcse3@gmail.com>
2022-08-05 20:32:39 +0100

    factorial solved

---
+++ factorial.js
@@ -0,0 +1,10 @@
+function factorial(number) {
+    let multiplication = 1;
+    for (let i = 1; i <= number; i++) {
+        multiplication = multiplication * i;
+    }
+    return multiplication;
+}
+
+const result = factorial(9);
+console.log(result);
